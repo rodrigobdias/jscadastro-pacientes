549620af875872038ee363d1b063ea4aa6c2fbcd
Rodrigo <rodrigobdias@gmail.com>
2019-02-21 10:09:35 -0300

    AjaxBuscando pacientes com AJAX

--- js/form.js
+++ js/form.js
@@ -8,8 +8,6 @@ botaoAdicionar.addEventListener("click", function(event){
 
     var paciente = obtemPacienteDoFormulario(form);
 
-    var pacienteTr = montaTr(paciente);
-
     var erros = validaPaciente(paciente);
 
     console.log(erros);
@@ -19,15 +17,21 @@ botaoAdicionar.addEventListener("click", function(event){
         return;
     }
  
-    var tabela = document.querySelector("#tabela-pacientes");
-
-    tabela.appendChild(pacienteTr);
-
+    adicionaPacienteNaTabela(paciente);
+    
     form.reset();
 
     var mensagensErro = document.querySelector("#mensagens-erro");
     mensagensErro.innerHTML = "";
 });
+
+
+function adicionaPacienteNaTabela(paciente) {
+    var pacienteTr = montaTr(paciente);
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+}
+
 
 function montaTr(paciente) {
     var pacienteTr = document.createElement("tr");
